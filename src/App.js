d4b1f235187cefda3465145850a2ba70a402f086
sohupatel8828 <sohupatel8828@gmail.com>
2023-09-03 12:34:25 -0700

    Add todo and get todo completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import {useMutation, useQuery} from '@apollo/react-hooks';
+
+import React from 'react';
+import {gql} from "apollo-boost";
+
+const GET_TODOS = gql `
+  query getTodos {
+    todos {
+      done
+      id
+      text
+    }
+  }
+  `;
+
+  const TOGGLE_TODO = gql `
+  mutation toggleTodo($id:uuid!, $done: Boolean!) {
+    update_todos(where: {id: {_eq: $id}}, _set: {done: $done}) {
+      returning {
+        done
+        id
+        text
+      }
+    }
+  }`;
+
+  const ADD_TODO = gql`
+  mutation addTodo($text:String!) {
+    insert_todos(objects: {text: $text}) {
+      returning {
+        done
+        id
+        text
+      }
+    }
+  }`
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+  const [todoText, setTodoText]= React.useState('')
+  const {data,loading,error} = useQuery(GET_TODOS)
+  const [toggleTodo] = useMutation(TOGGLE_TODO)
+  const [addTodo] = useMutation(ADD_TODO, {onCompleted: ()=> setTodoText('')});
+  
+  async function handleToggleTodo({id,done}){
+    const data = await toggleTodo({ variables: { id: id, done: !done }})
+    console.log('toggled todo',data);
+  }
+
+  async function handleAddTodo(event){
+    event.preventDefault();
+    if (!todoText.trim()) return;
+    const data = await addTodo({
+      variables:{text:todoText}, 
+      refetchQueries: [{ query:GET_TODOS }]}
+      )
+    console.log('added todo',data)
+    // setTodoText('');
+  }
+
+  if (loading) return <div>Loading...</div>
+  if (error) return <p>Error : {error.message}</p>;
+  return ( 
+  <div className="vh-100 code flex-column items-center justify-center bg-blue white pa3 fl-1">
+    <h1 className="f2-1 tc">GraphQL Checklist{" "}<span role="img" aria-label="Checkmark">✅</span></h1>
+    {/* todo form */}
+    <form onSubmit={handleAddTodo} className="flex justify-center">
+      <input type="text" placeholder="write your todo" onChange={event => setTodoText(event.target.value)} value={todoText} className="pa2 f4 ba b--black br3"></input>
+      <button type="submit" className="pa2 ml2 f4 bg-green ba br3">Create</button>
+    </form>
+    {/* Todo list */}
+    <div className="flex items-center justify-center flex-column">
+    {
+      data.todos.map(todo => (
+        <p key={todo.id}>
+          <input type='checkbox' onChange={() => handleToggleTodo(todo)} ></input>
+          <span className={`pointer list pa1 f3 ${todo.done && 'strike'}`}>{todo.text}</span>
+          <button className="bg-transparent bn f3 br2"><span className="dim red">&times;</span></button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    ))}
     </div>
-  );
+  </div>);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
+import {ApolloClient, ApolloProvider, InMemoryCache, gql} from "@apollo/client";
+
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const secret = process.env.REACT_APP_HASURA_ADMIN_SECRET
+
+const client = new ApolloClient({ uri : 'https://large-magpie-41.hasura.app/v1/graphql',
+cache: new InMemoryCache(),
+headers: {
+  "x-hasura-admin-secret": secret ,
+},})
+
+client.query({
+  query: gql `
+  query getTodos {
+    todos {
+      done
+      id
+      text
+    }
+  }
+  `
+}).then(data => console.log(data))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+  <App />
+  </ApolloProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
